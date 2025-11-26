88b28a375ae863cc1aa8195e9a9e2571c383aa78
ALESEV <alexdidu21@gmail.com>
2025-11-26 12:56:30 +0200

    Created footer for web-app

---
+++ resources/views/app/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="fixed bottom-0 left-[70px] h-[25px] w-[calc(100vw-70px)] bg-orange-700/80 flex flex-col items-center  justify-center">
+    <p class="text-white text-sm font-bold">You're viewing a sample company.
+When you're ready, <a href="" class="underline hover:no-underline">switch to your company.</a></p>
+</div>
--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -18,6 +18,8 @@
         @yield("main")
     </main>
 
+    @include('app.footer')
+
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </body>

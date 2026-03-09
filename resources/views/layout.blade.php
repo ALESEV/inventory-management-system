14febd647214b9e6c56a534517b353400a6c0221
ALESEV <alexdidu21@gmail.com>
2026-03-09 23:31:19 +0200

    Test Workflow

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nova Inventory - Inventory Management System</title>
+    <title>Nova Flow - Your Management System</title>
 
     <link rel="icon" type="image/png" href="{{ asset('storage/logo.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
